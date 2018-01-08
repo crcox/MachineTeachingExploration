90033600fb0f979556d52dc1f3d9359a7ae31368
Chris Cox <christopher.cox-2@mancheste.ac.uk>
2018-01-08 08:48:41 +0000

    sync

--- linearRegressionGD.R
+++ linearRegressionGD.R
@@ -37,7 +37,7 @@ linearRegressionSGD<- function(y, X, epsilon = 0.0001, eta = 10, iters = 1000){
         ix <- sample(N)
         for (j in ix) {
             e = y[j] - matrix(theta,nrow=1)%*%matrix(X[j,],ncol=1)
-            grad = -(2/N)%*%e%*%X[j,]
+            grad = -(2)%*%e%*%X[j,]
             theta = theta - eta*(2/N)*grad
             if(sqrt(sum(grad^2)) <= epsilon){
                 break
@@ -65,13 +65,13 @@ linearRegressionSGD_Candide <- function(y, X, epsilon = 0.0001, eta = 10, iters 
         l2loss_inner = rep(0,N)
         for (j in 1:N) {
             e = y[j] - matrix(theta,nrow=1)%*%matrix(X[j,],ncol=1)
-            grad = -(2/N)%*%e%*%X[j,]
+            grad = -(2)%*%e%*%X[j,]
             theta_tmp = theta - eta*(2/N)*grad
             l2loss_inner[j] = sqrt(sum((t(y) - theta_tmp%*%t(X))^2))
         }
         k <- which.min(l2loss_inner)
         e = y[k] - matrix(theta,nrow=1)%*%matrix(X[k,],ncol=1)
-        grad = -(2/N)%*%e%*%X[k,]
+        grad = -(2)%*%e%*%X[k,]
         theta = theta - eta*(2/N)*grad
         l2loss$example[i] = k
         l2loss$loss[i] = sqrt(sum((t(y) - theta%*%t(X))^2))
@@ -86,30 +86,55 @@ linearRegressionSGD_Candide <- function(y, X, epsilon = 0.0001, eta = 10, iters 
     return(values)
 }
 
-linearRegressionSGD_Smith <- function(y, X, epsilon = 0.0001, eta = 10, iters = 1000){
+
+### SMITH
+omniscient_loss <- function(grad,theta,teacher,eta) {
+    difficulty <- sqrt(sum(grad^2))
+    usefulness <- matrix((theta - teacher),nrow=1) %*% matrix(grad,ncol=1)
+    obj <- (eta^2 * difficulty) - (2 * eta * usefulness)
+    return(obj)
+}
+linearRegressionSGD_Smith <- function(y, X, teacher, epsilon = 0.0001, eta = 100, iters = 1000){
+    # Teacher will be the solution from the standard closed-form linear regression (lm).
+    if (is.null(teacher)) {
+        teacher <- coef(lm(y~X))
+    }
     X = as.matrix(data.frame(rep(1,length(y)),X))
+    names(X)[1] <- '(Intercept)'
     N = dim(X)[1]
+    if (dim(X)[2] > length(teacher)) {
+        teacher <- c(0,teacher)
+    }
+    names(teacher)[0] <- '(Intercept)'
     print("Initialize parameters...")
     theta.init = as.matrix(rnorm(n=dim(X)[2], mean=0,sd = 1)) # Initialize theta
     theta.init = t(theta.init)
     e = t(y) - theta.init%*%t(X)
     grad.init = -(2/N)%*%(e)%*%X
     theta = theta.init - eta*(1/N)*grad.init
-    l2loss = c()
-    for(i in 1:iters){
-        ix <- sample(N)
-        for (j in ix) {
+    l2loss = data.frame(example=numeric(iters*N),loss=numeric(iters*N))
+    for(i in 1:(iters*N)) {
+        obj <- rep(0, N)
+        for (j in 1:N) {
             e = y[j] - matrix(theta,nrow=1)%*%matrix(X[j,],ncol=1)
-            grad = -(2/N)%*%e%*%X[j,]
-            theta = theta - eta*(2/N)*grad
-            if(sqrt(sum(grad^2)) <= epsilon){
-                break
-            }
+            grad = -(2)%*%e%*%X[j,]
+            obj[j] <- omniscient_loss(grad,theta,teacher,eta)
         }
-        l2loss = c(l2loss,sqrt(sum((t(y) - theta%*%t(X))^2)))
+        k <- which.min(obj)
+        e = y[k] - matrix(theta,nrow=1)%*%matrix(X[k,],ncol=1)
+        grad = -(2)%*%e%*%X[k,]
+        theta = theta - eta*(2/N)*grad
+        l2loss$example[i] = k
+        l2loss$loss[i] = sqrt(sum((t(y) - theta%*%t(X))^2))
+        if(sqrt(sum(grad^2)) <= epsilon){
+            break
+        }
     }
+    l2loss <- l2loss[l2loss$example!=0,]
     print("Algorithm converged")
     print(paste("Final gradient norm is",sqrt(sum(grad^2))))
     values<-list("coef" = t(theta), "l2loss" = l2loss)
     return(values)
 }
+
+
--- linearRegressionGD_test.R
+++ linearRegressionGD_test.R
@@ -5,4 +5,5 @@ y <- X%*%theta_0
 m0 <- linearRegressionGD(y,X)
 m1 <- linearRegressionSGD(y,X)
 m2 <- linearRegressionSGD_Candide(y,X)
-m3 <- linearRegressionSGD_Smith(y,X)
+m3 <- linearRegressionSGD_Smith(y,X,theta_0)
+m4 <- linearRegressionSGD_Smith(y,X,theta_0,omniscient=FALSE)
